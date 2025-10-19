timangus/ioq3

XDG home directory support

code/qcommon/q_shared.h
@@ -31,8 +31,9 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
   #define BASEGAME					"foobar"
   #define CLIENT_WINDOW_TITLE		"changeme"
   #define CLIENT_WINDOW_MIN_TITLE	"changeme2"
-  #define HOMEPATH_NAME_UNIX		".foo"
+  #define HOMEPATH_NAME_UNIX_LEGACY	".foo"
   #define HOMEPATH_NAME_WIN			"FooBar"
+  #define HOMEPATH_NAME_UNIX		HOMEPATH_NAME_WIN
   #define HOMEPATH_NAME_MACOSX		HOMEPATH_NAME_WIN
   #define GAMENAME_FOR_MASTER		"foobar"	// must NOT contain whitespace
   #define CINEMATICS_LOGO		"foologo.roq"
@@ -44,8 +45,9 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
   #define BASEGAME					"baseq3"
   #define CLIENT_WINDOW_TITLE		"ioquake3"
   #define CLIENT_WINDOW_MIN_TITLE	"ioq3"
-  #define HOMEPATH_NAME_UNIX		".q3a"
+  #define HOMEPATH_NAME_UNIX_LEGACY	".q3a"
   #define HOMEPATH_NAME_WIN			"Quake3"
+  #define HOMEPATH_NAME_UNIX		HOMEPATH_NAME_WIN
   #define HOMEPATH_NAME_MACOSX		HOMEPATH_NAME_WIN
   #define GAMENAME_FOR_MASTER		"Quake3Arena"
   #define CINEMATICS_LOGO		"idlogo.RoQ"

pstngh/openmohaa-client

Co-authored-by: smallmodel <15067410+smallmodel@users.noreply.github.com>

code/qcommon/q_shared.h
@@ -77,9 +77,8 @@ extern "C" {
 // Note: An universal client is not currently possible without refactoring the network and the file system.
 //       Pak files must be reloaded on-the-fly depending on the server the client is connecting to.
 //
-#define HOMEPATH_NAME_UNIX		".openmohaa"
-#define HOMEPATH_NAME_WIN		"openmohaa"
-#define HOMEPATH_NAME_MACOSX	HOMEPATH_NAME_WIN
+ #define HOMEPATH_NAME_UNIX_LEGACY	".openmohaa"
+ #define HOMEPATH_NAME				"openmohaa"
 
 #define CONFIG_PREFIX			"opmconfig"
 